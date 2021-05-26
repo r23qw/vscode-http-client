73341265cc5d97a6be43dfde64c4233daf3b6f2b
aaron00101010 <aaron00101010@gmail.com>
2021-05-26 22:21:56 +0800

    chore: stash

--- src/views/components/Request/TabsBar/NewMenu.tsx
+++ src/views/components/Request/TabsBar/NewMenu.tsx
@@ -1,34 +1,21 @@
-import { Menu, Modal } from "antd";
-import React, { useState } from "react";
+import { Input, Menu, Modal } from "antd";
+import React from "react";
 
 export default function NewMenu() {
-  const [visible, setVisible] = useState(false);
-
   const handleMenuClick = ({ key }: any) => {
-    if (key === "curl") {
-      setVisible(true);
-    }
+    if (key !== "curl") return;
+    Modal.confirm({
+      icon: null,
+      content: <Input />,
+      onOk() {},
+    });
   };
 
-  const handleConfirm = () => {
-    setVisible(false);
-  };
-  const handleCancel = () => {
-    setVisible(false);
-  };
   return (
     <>
       <Menu onClick={handleMenuClick}>
         <Menu.Item key="curl">import by bash curl</Menu.Item>
       </Menu>
-      <Modal
-        title="import request by bash curl"
-        visible={visible}
-        onOk={handleConfirm}
-        onCancel={handleCancel}
-      >
-        test
-      </Modal>
     </>
   );
 }
