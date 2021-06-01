15ed22e041cdabb05334858de52021d0a3bbe158
aaron00101010 <aaron00101010@gmail.com>
2021-06-01 17:01:23 +0800

    feat: response status

--- src/views/components/Request/ResponsePanel/index.tsx
+++ src/views/components/Request/ResponsePanel/index.tsx
@@ -1,3 +1,4 @@
+import { useRequestSelector } from "@/store";
 import { Tabs } from "antd";
 import ErrorBoundary from "antd/lib/alert/ErrorBoundary";
 import React from "react";
@@ -7,19 +8,40 @@ import styles from "./index.module.css";
 import ResponseBody from "./ResponseBody";
 
 export default function ResponsePanel() {
+  const request = useRequestSelector();
+  const response = request.response;
+  const { status, statusText } = response;
+  const Status = () => {
+    return status === null ? null : (
+      <div className={styles.status}>
+        <span
+          className={`${styles.dot} ${
+            status >= 200 && status < 300 ? styles.success : styles.fail
+          }`}
+        />
+        &nbsp;
+        {status}
+        &nbsp;
+        {statusText}
+      </div>
+    );
+  };
   return (
-    <Tabs animated={false} className={styles.container}>
-      <Tabs.TabPane tab="Body" key="body">
-        <ErrorBoundary>
-          <ResponseBody />
-        </ErrorBoundary>
-      </Tabs.TabPane>
-      <Tabs.TabPane tab="Headers" key="headers">
-        <Headers />
-      </Tabs.TabPane>
-      <Tabs.TabPane tab="Cookies" key="cookies">
-        <Cookies />
-      </Tabs.TabPane>
-    </Tabs>
+    <>
+      <Status />
+      <Tabs animated={false} className={styles.container}>
+        <Tabs.TabPane tab="Body" key="body">
+          <ErrorBoundary>
+            <ResponseBody />
+          </ErrorBoundary>
+        </Tabs.TabPane>
+        <Tabs.TabPane tab="Headers" key="headers">
+          <Headers />
+        </Tabs.TabPane>
+        <Tabs.TabPane tab="Cookies" key="cookies">
+          <Cookies />
+        </Tabs.TabPane>
+      </Tabs>
+    </>
   );
 }
