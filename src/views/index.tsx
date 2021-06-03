8f821d03a374b664621f9f90680f8ba1bf81342d
aaron00101010 <aaron00101010@gmail.com>
2021-06-03 10:14:34 +0800

    refactor: activate extension when vscode start up finished

--- src/views/index.tsx
+++ src/views/index.tsx
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { isVscodeWebview } from "./utils/env";
 
-const noop = () => {};
-
 window.vscodeRef = isVscodeWebview ? window.acquireVsCodeApi() : undefined;
 
 ReactDOM.render(<App />, document.getElementById("app"));
